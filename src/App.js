af1339aa0e8c7422a7c795eb4c1a0addca10fef0
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-23 12:18:57 -0700

    created components for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./style/master.scss"; // applies global scss styles
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; //importing then using "as" to name it
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-danger">Hello World</h1>
-    </div>
-  );
+   return (
+      //wrapping everything in a router
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/create-answer" component={CreateAnswer} />
+            <Route exact path="/create-imagery" component={CreateImagery} />
+            <Route exact path="/review-imagery" component={ReviewImagery} />
+            <Route exact path="/review-answer" component={ReviewAnswer} />
+            <Route exact path="/review-empty" component={ReviewEmpty} />
+            <Route exact path="/all-cards" component={AllCards} />
+            <Route exact path="/edit" component={Edit} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
