d0afec0156aa47fbdef1d5240884eef726ad8e20
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-24 10:00:45 -0700

    finding tab active

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,33 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
+   const url = window.location.pathname;
+   console.log(url);
+   const tabActiveOnCreate = (url) => {
+      if (
+         url.indexOf("create-imagery") > 0 ||
+         url.indexOf("create-answer") > 0
+      ) {
+         return "tab-active";
+      } else return "";
+   };
+   const tabActiveOnReview = (url) => {
+      if (
+         url.indexOf("review-imagery") > 0 ||
+         url.indexOf("review-answer") > 0 ||
+         url.indexOf("review-empty") > 0
+      ) {
+         return "tab-active";
+      } else return "";
+   };
+   const tabActiveOnAllCards = (url) => {
+      if (url.indexOf("all-cards") > 0) {
+         return "tab-active";
+      } else return "";
+   };
+
    return (
       <div
          className="btn-group mb-5 mt-1 d-flex "
          role="navigation"
          aria-label="navigation"
       >
-         <a
-            href="create-answer.html"
-            role="button"
-            className="btn btn-secondary"
+         <Link
+            to="/create-answer"
+            type="button"
+            className={`btn btn-secondary tab-separator ${tabActiveOnCreate(
+               url
+            )}`}
          >
             Create new
-         </a>
-         <a
-            href="review-imagery.html"
-            role="button"
-            className="btn btn-secondary tab-separator"
+         </Link>
+         <Link
+            to="/review-imagery"
+            type="button"
+            className={`btn btn-secondary tab-separator ${tabActiveOnReview(
+               url
+            )}`}
          >
             Review
-         </a>
-         <a
-            href="all-cards.html"
-            role="button"
-            className="btn btn-secondary tab-separator"
+         </Link>
+         <Link
+            to="/all-cards"
+            type="button"
+            className={`btn btn-secondary tab-separator ${tabActiveOnAllCards(
+               url
+            )}`}
          >
             All cards
-         </a>
+         </Link>
       </div>
    );
 }
