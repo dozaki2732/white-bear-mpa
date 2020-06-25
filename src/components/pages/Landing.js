62f32089a8abe5547b713d71b0be85eff3b0ceea
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-25 11:06:23 -0700

    sign up card for landing

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,5 +1,10 @@
 import React from "react";
+import SignUpCard from "../ui/SignUpCard";
 
 export default function Landing() {
-   return <></>;
+   return (
+      <>
+         <SignUpCard />
+      </>
+   );
 }
