e33ff79a13688fa6ad90d5183d2c766a61e57aa4
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-29 16:29:35 -0700

    validating email address

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -7,14 +7,14 @@ export default function Landing() {
    return (
       <>
          <div className="landing-pg-bg">
-            <div class="row mb-8 pt-7 ml-2 justify-content-left">
-               <div class="d-flex mt-5">
+            <div className="row mb-8 pt-7 ml-2 justify-content-left">
+               <div className="d-flex mt-5">
                   <img
-                     class="img-fluid"
+                     className="img-fluid"
                      src={LandingLogo}
                      alt="white bear constellation"
                   />
-                  <h1 class="logo-font d-line ml-5">White Bear</h1>
+                  <h1 className="logo-font d-line ml-5">White Bear</h1>
                </div>
             </div>
             <div className="row">
--- src/components/ui/SignUpCard.js
+++ src/components/ui/SignUpCard.js
@@ -1,47 +1,121 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import classnames from "classnames";
 
-export default function SignUpCard() {
-   return (
-      <div className="offset-1 col-10 offset-sm-1 col-sm-9 offset-md-1 col-md-4 offset-lg-2 col-lg-4 offset-xl-2 col-xl-4 mb-6">
-         <div className="card">
-            <div className="card-body">
-               <div className="landing-card">
-                  <h2>Nice to meet you</h2>
-                  <p className="card-text mb-5">
-                     Sign up for White Bear, Free Forever.
-                  </p>
+export default class SignUpCard extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log("new");
+      this.state = {
+         isDisplayingInputs: false,
+         emailError: "",
+         passwordError: "",
+         hasEmailError: false,
+      };
+   }
 
-                  <Link
-                     to="/create-answer"
-                     role="button"
-                     className="btn btn-landing btn-success btn-block"
-                  >
-                     Sign up
-                  </Link>
-                  <p className="signup-text mb-4">Lets get you signed up</p>
-                  <div className="form-group">
-                     <label className="text-muted">Email address</label>
-                     <input type="email" className="form-control" required />
+   showInputs() {
+      this.setState({
+         isDisplayingInputs: true,
+      });
+   }
+
+   validateAndCreateUser() {
+      const emailInput = document.getElementById("email-input").value;
+      //eslint-disable-next-line
+      const loweredCasedEmailInput = emailInput.toLowerCase();
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (emailInput === "")
+         this.setState({
+            emailError: "Please enter your email address",
+            hasEmailError: true,
+         });
+      //changing state from empty string to error message
+      else if (emailRegex.test(loweredCasedEmailInput) === false) {
+         this.setState({
+            emailError: "Please enter a valid email address",
+            hasEmailError: true,
+         });
+      } else {
+         this.setState({ emailError: "", hasEmailError: false });
+      }
+   }
+
+   render() {
+      return (
+         <div className="offset-1 col-10 offset-sm-1 col-sm-9 offset-md-1 col-md-4 offset-lg-2 col-lg-4 offset-xl-2 col-xl-4 mb-6">
+            <div className="card">
+               <div className="card-body">
+                  <div className="landing-card">
+                     <h2>Nice to meet you</h2>
+                     <p className="card-text mb-5">
+                        Sign up for White Bear, Free Forever.
+                     </p>
+                     {!this.state.isDisplayingInputs && (
+                        <button
+                           className="btn btn-landing btn-success btn-block"
+                           onClick={() => {
+                              this.showInputs();
+                           }}
+                        >
+                           Sign up
+                        </button>
+                     )}
+
+                     {this.state.isDisplayingInputs && (
+                        <>
+                           <p className="signup-text mb-4">
+                              Lets get you signed up
+                           </p>
+                           <div className="form-group">
+                              <label
+                                 className="text-muted"
+                                 htmlFor="email-input"
+                              >
+                                 Email address
+                              </label>
+                              <input
+                                 type="email"
+                                 className={classnames({
+                                    "form-control": true,
+                                    "is-invalid": this.state.hasEmailError,
+                                 })}
+                                 id="email-input"
+                              />
+                              {this.state.hasEmailError !== "" && (
+                                 <p className="text-danger">
+                                    {this.state.emailError}
+                                 </p>
+                              )}
+                           </div>
+                           <div className="form-group">
+                              <label
+                                 className="text-muted"
+                                 htmlFor="password-input"
+                              >
+                                 Create a password
+                              </label>
+                              <input
+                                 type="password"
+                                 className="form-control"
+                                 id="password-input"
+                              />
+                           </div>
+                           <button
+                              className="btn btn-success btn-landing btn-block mt-5"
+                              to="/create-answer"
+                              onClick={() => {
+                                 this.validateAndCreateUser();
+                              }}
+                           >
+                              Lets go!
+                           </button>
+                        </>
+                     )}
                   </div>
-                  <div className="form-group">
-                     <label className="text-muted">Create a password</label>
-                     <input
-                        type="password"
-                        className="form-control"
-                        id="inputPassword"
-                     />
-                  </div>
-                  <Link
-                     className="btn btn-success btn-landing btn-block mt-5"
-                     role="button"
-                     to="/create-answer"
-                  >
-                     Lets go!
-                  </Link>
                </div>
             </div>
          </div>
-      </div>
-   );
+      );
+   }
 }
