33b12f8298fc3e02f8efd4927a68427c935d52f8
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-07-01 16:17:34 -0700

    char counter on edit

--- src/components/pages/CreateAnswer.js
+++ src/components/pages/CreateAnswer.js
@@ -3,29 +3,50 @@ import Header from "../ui/Header";
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
+import { checkIsOver, MAX_CARD_CHARS } from "../../utils/helpers";
 
-export default function CreateAnswer() {
-   return (
-      <AppTemplate>
-         <Header />
-         <Navigation />
-         <h4 className="text-center text-muted mb-3"> Add an Answer</h4>
-         <div className="card">
-            <div className="mb-5 bg-primary card-body">
-               <textarea autoFocus={true} rows="11"></textarea>
+export default class CreateAnswer extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         answerText: "",
+      };
+   }
+
+   checkHasInvalidCharCount() {
+      if (
+         this.state.answerText.length > MAX_CARD_CHARS ||
+         this.state.answerText.length === 0
+      ) {
+         return true;
+      } else return false;
+   }
+
+   setAnswerText(e) {
+      this.setState({ answerText: e.target.value });
+   }
+
+   render() {
+      return (
+         <AppTemplate>
+            <Header />
+            <Navigation />
+            <h4 className="text-center text-muted mb-3"> Add an Answer</h4>
+            <div className="card">
+               <div className="mb-5 bg-primary card-body">
+                  <textarea autoFocus={true} rows="11"></textarea>
+               </div>
             </div>
-         </div>
-         <div className="float-right mb-5">
-            <span id="charNum"> 0 </span> /240
-         </div>
-         <div className="clearfix"></div>
+            <div className="float-right mb-5">0 / {MAX_CARD_CHARS}</div>
+            <div className="clearfix"></div>
 
-         <Link
-            to="/create-imagery"
-            className="btn btn-outline-primary btn-lg float-right"
-         >
-            Next
-         </Link>
-      </AppTemplate>
-   );
+            <Link
+               to="/create-imagery"
+               className="btn btn-outline-primary btn-lg float-right"
+            >
+               Next
+            </Link>
+         </AppTemplate>
+      );
+   }
 }
--- src/components/pages/Edit.js
+++ src/components/pages/Edit.js
@@ -6,130 +6,178 @@ import { Link } from "react-router-dom";
 import saveIcon from "../../icons/save.svg";
 import memoryCards from "../../mock-data/memory-cards";
 import toDisplayDate from "date-fns/format";
+import classnames from "classnames";
+import { checkIsOver, MAX_CARD_CHARS } from "../../utils/helpers";
 
 const memoryCard = memoryCards[2];
 
-export default function Edit() {
-   return (
-      <AppTemplate>
-         <Header />
-         <Navigation />
-         <h4 className="text-center text-muted mt-3 mb-3">Edit Card </h4>
-         <div className="card">
-            <div className="card-body bg-primary lead">
-               <textarea
-                  className="d-md-none"
-                  rows="7"
-                  defaultValue={memoryCard.imagery}
-               ></textarea>
+export default class Edit extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log("edit");
+      this.state = {
+         answerText: memoryCard.answer,
+         imageryText: memoryCard.imagery,
+      };
+   }
 
-               <textarea
-                  className="d-non d-md-block"
-                  rows="4"
-                  defaultValue={memoryCard.imagery}
-               ></textarea>
+   checkHasInvalidCharCount() {
+      if (
+         this.state.answerText.length > MAX_CARD_CHARS ||
+         this.state.imageryText.length > MAX_CARD_CHARS ||
+         this.state.answerText.length === 0 ||
+         this.state.imageryText.length === 0
+      ) {
+         return true;
+      } else return false;
+   }
+
+   setImageryText(e) {
+      this.setState({ imageryText: e.target.value });
+   }
+
+   setAnswerText(e) {
+      this.setState({ answerText: e.target.value });
+   }
+
+   render() {
+      return (
+         <AppTemplate>
+            <Header />
+            <Navigation />
+            <h4 className="text-center text-muted mt-3 mb-3">Edit Card </h4>
+            <div className="card">
+               <div className="card-body bg-primary lead">
+                  <textarea
+                     className=" d-md-block"
+                     rows="4"
+                     defaultValue={memoryCard.imagery}
+                     onChange={(e) => this.setImageryText(e)}
+                  ></textarea>
+               </div>
             </div>
-         </div>
-         <div className="card">
-            <div className="card-body bg-primary lead">
-               <textarea
-                  className="d-md-none"
-                  rows="7"
-                  defaultValue={memoryCard.answer}
-               ></textarea>
-
-               <textarea
-                  className="d-non d-md-block"
-                  rows="4"
-                  defaultValue={memoryCard.answer}
-               ></textarea>
+            <div className="card">
+               <div className="card-body mb-2 bg-secondary lead">
+                  <textarea
+                     className="d-md-block"
+                     rows="4"
+                     defaultValue={memoryCard.answer}
+                     onChange={(e) => this.setAnswerText(e)}
+                  ></textarea>
+               </div>
             </div>
-         </div>
-         <div className="container">
-            <div className="row">
-               <div className="mb-5">
-                  <Link to="/all-cards" className="btn btn-link">
-                     Discard changes
-                  </Link>
-
-                  <Link
-                     to="/all-cards"
-                     role="button"
-                     className="btn btn-primary btn-lg "
-                     id="save-imagery"
+            <div className="float-right mb-5">
+               <p>
+                  <span
+                     className={classnames({
+                        "text-danger": checkIsOver(
+                           this.state.imageryText,
+                           MAX_CARD_CHARS
+                        ),
+                     })}
                   >
-                     <img
-                        alt=""
-                        src={saveIcon}
-                        width="20px"
-                        style={{ marginBottom: "3px", marginRight: "4px" }}
-                     />
-                     Save
-                  </Link>
-               </div>
+                     Top:&nbsp;
+                     {this.state.imageryText.length}/{MAX_CARD_CHARS}
+                  </span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;{" "}
+                  <span
+                     className={classnames({
+                        "text-danger": checkIsOver(
+                           this.state.answerText,
+                           MAX_CARD_CHARS
+                        ),
+                     })}
+                  >
+                     Bottom:
+                     {this.state.answerText.length}/{MAX_CARD_CHARS}
+                  </span>
+               </p>
             </div>
-         </div>
-         <div>
-            <div className="text-center text-muted my-6">
-               <h4>Card properties</h4>
-            </div>
-            <div className="row mb-2">
-               <div className="col-4">
-                  <p className="text-muted">Created on:</p>
-               </div>
-               <div className="col-8">
-                  <p> {toDisplayDate(memoryCard.createdAt, "MMM. d, yyy")} </p>
-               </div>
-            </div>
-            <div className="row mb-2">
-               <div className="col-4">
-                  <p className="text-muted">Last attempt:</p>
-               </div>
-               <div className="col-8">
-                  <p>
-                     {toDisplayDate(memoryCard.lastAttemptAt, "MMM. d, yyy")}
-                  </p>
-               </div>
-            </div>
-            <div className="row mb-2">
-               <div className="col-4">
-                  <p className="text-muted">Next attempt:</p>
-               </div>
-               <div className="col-8">
-                  <p>
-                     {toDisplayDate(memoryCard.nextAttemptAt, "MMM. d, yyy")}
-                  </p>
-               </div>
-            </div>
-            <div className="row mb-4">
-               <div className="col-4">
-                  <p className="text-muted">Consecutives:</p>
-               </div>
-               <div className="col-8">
-                  <p>{memoryCard.totalSuccessfulAttempts}</p>
-               </div>
-            </div>
-            <div className="row col mb-4">
-               <div className="custom-control custom-checkbox text-muted">
-                  <input
-                     type="checkbox"
-                     className="custom-control-input"
-                     id="delete-check"
-                  />
-                  <label className="custom-control-label">
-                     Show delete button
-                  </label>
-               </div>
-            </div>
+            <div className="clearfix"></div>
+
+            <Link to="/all-cards" className="btn btn-link">
+               Discard changes
+            </Link>
+
             <Link
                to="/all-cards"
-               id="card-delete"
-               href="all-cards.html"
-               className="btn btn-large btn-outline-danger  mb-3"
+               role="button"
+               className={classnames("btn btn-primary btn-lg float-right", {
+                  disabled: this.checkHasInvalidCharCount(),
+               })}
+               id="save-imagery"
             >
-               Delete this card
+               <img
+                  alt=""
+                  src={saveIcon}
+                  width="20px"
+                  style={{ marginBottom: "3px", marginRight: "4px" }}
+               />
+               Save
             </Link>
-         </div>
-      </AppTemplate>
-   );
+
+            <div>
+               <div className="text-center text-muted my-6">
+                  <h4>Card properties</h4>
+               </div>
+               <div className="row mb-2">
+                  <div className="col-4">
+                     <p className="text-muted">Created on:</p>
+                  </div>
+                  <div className="col-8">
+                     <p>{toDisplayDate(memoryCard.createdAt, "MMM. d, yyy")}</p>
+                  </div>
+               </div>
+               <div className="row mb-2">
+                  <div className="col-4">
+                     <p className="text-muted">Last attempt:</p>
+                  </div>
+                  <div className="col-8">
+                     <p>
+                        {toDisplayDate(memoryCard.lastAttemptAt, "MMM. d, yyy")}
+                     </p>
+                  </div>
+               </div>
+               <div className="row mb-2">
+                  <div className="col-4">
+                     <p className="text-muted">Next attempt:</p>
+                  </div>
+                  <div className="col-8">
+                     <p>
+                        {toDisplayDate(memoryCard.nextAttemptAt, "MMM. d, yyy")}
+                     </p>
+                  </div>
+               </div>
+               <div className="row mb-4">
+                  <div className="col-4">
+                     <p className="text-muted">Consecutives:</p>
+                  </div>
+                  <div className="col-8">
+                     <p>{memoryCard.totalSuccessfulAttempts}</p>
+                  </div>
+               </div>
+               <div className="row col mb-4">
+                  <div className="custom-control custom-checkbox text-muted">
+                     <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id="delete-check"
+                     />
+                     <label className="custom-control-label">
+                        Show delete button
+                     </label>
+                  </div>
+               </div>
+               <Link
+                  to="/all-cards"
+                  id="card-delete"
+                  href="all-cards.html"
+                  className="btn btn-large btn-outline-danger  mb-3"
+               >
+                  Delete this card
+               </Link>
+            </div>
+         </AppTemplate>
+      );
+   }
 }
