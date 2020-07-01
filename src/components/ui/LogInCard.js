aebc04a464b145c3f0a944c8b10b4e2c953aa49f
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-07-01 12:45:54 -0700

    validated and created users

--- src/components/ui/LogInCard.js
+++ src/components/ui/LogInCard.js
@@ -1,47 +1,132 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import classnames from "classnames";
+import hash from "object-hash";
+import { v4 as getUuid } from "uuid";
 
-export default function LogInCard() {
-   return (
-      <div className="offset-1 col-10 offset-sm-1 col-sm-9 offset-md-1 col-md-4 offset-lg-1 col-lg-4 offset-xl-1 col-xl-4">
-         <div className="card">
-            <div className="card-body">
-               <div className="landing-card">
-                  <h2>Welcome back!</h2>
-                  <p className="card-text mb-4">
-                     Log in with your email address and password
-                  </p>
-                  <div className="form-group">
-                     <label className="input-text" for="email">
-                        Email address
-                     </label>
-                     <input
-                        type="email"
-                        className="form-control"
-                        id="emailText"
-                     />
-                  </div>
+export default class SignUpCard extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log("new");
+      this.state = {
+         emailError: "",
+         passwordError: false,
+         hasEmailError: false,
+      };
+   }
 
-                  <div className="form-group">
-                     <label className="input-text" for="password">
-                        Password
-                     </label>
-                     <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                     />
+   setEmailState(emailInput) {
+      const loweredCasedEmailInput = emailInput.toLowerCase();
+      //eslint-disable-next-line
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (emailInput === "")
+         this.setState({
+            emailError: "Please enter your email address",
+            hasEmailError: true,
+         });
+      //changing state from empty string to error message
+      else if (emailRegex.test(loweredCasedEmailInput) === false) {
+         this.setState({
+            emailError: "Please enter a valid email address",
+            hasEmailError: true,
+         });
+      } else {
+         this.setState({ emailError: "", hasEmailError: false });
+      }
+   }
+
+   setPasswordState(passwordInput) {
+      if (passwordInput === "") {
+         this.setState({
+            passwordError: "Please enter your password.",
+            hasPasswordError: true,
+         });
+      } else {
+         this.setState({ passwordError: "", hasPasswordError: false });
+      }
+   }
+
+   validateAndLogUser() {
+      const emailInput = document.getElementById("email-input").value;
+      const passwordInput = document.getElementById("password-input").value;
+      this.setEmailState(emailInput);
+      this.setPasswordState(passwordInput, emailInput);
+      if (
+         this.state.hasEmailError === false &&
+         this.state.hasPasswordError === false
+      ) {
+         const user = {
+            id: getUuid(),
+            email: emailInput,
+            password: hash(passwordInput),
+            createdAt: Date.now(),
+         };
+         console.log(user);
+      }
+   }
+
+   render() {
+      return (
+         <div className="offset-1 col-10 offset-sm-1 col-sm-9 offset-md-1 col-md-4 offset-lg-1 col-lg-4 offset-xl-1 col-xl-4">
+            <div className="card">
+               <div className="card-body">
+                  <div className="landing-card">
+                     <h2>Welcome back!</h2>
+                     <p className="card-text mb-4">
+                        Log in with your email address and password
+                     </p>
+                     <div className="form-group">
+                        <label className="input-text" htmlFor="email">
+                           Email address
+                        </label>
+                        <input
+                           type="email"
+                           className={classnames({
+                              "mb-2": true,
+                              "form-control": true,
+                              "is-invalid": this.state.hasEmailError,
+                           })}
+                           id="email-input"
+                        />
+                        {this.state.hasEmailError !== "" && (
+                           <p className="text-danger">
+                              {this.state.emailError}
+                           </p>
+                        )}
+                     </div>
+
+                     <div className="form-group">
+                        <label className="input-text" htmlFor="password">
+                           Password
+                        </label>
+                        <input
+                           type="password"
+                           className={classnames({
+                              "mb-2": true,
+                              "form-control": true,
+                              "is-invalid": this.state.hasPasswordError,
+                           })}
+                           id="password-input"
+                        />
+                        {this.state.hasPasswordError !== "" && (
+                           <p className="text-danger">
+                              {this.state.passwordError}
+                           </p>
+                        )}
+                     </div>
+                     <button
+                        className="btn btn-success btn-landing btn-block mt-5"
+                        to="/create-answer"
+                        onClick={() => {
+                           this.validateAndLogUser();
+                        }}
+                     >
+                        Log in
+                     </button>
                   </div>
-                  <Link
-                     to="/create-answer"
-                     role="button"
-                     className="btn btn-success btn-lg btn-landing float-right"
-                  >
-                     Log in
-                  </Link>
                </div>
             </div>
          </div>
-      </div>
-   );
+      );
+   }
 }
--- src/components/ui/SignUpCard.js
+++ src/components/ui/SignUpCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classnames from "classnames";
+import hash from "object-hash";
+import { v4 as getUuid } from "uuid";
 
 export default class SignUpCard extends React.Component {
    constructor(props) {
@@ -8,7 +10,7 @@ export default class SignUpCard extends React.Component {
       this.state = {
          isDisplayingInputs: false,
          emailError: "",
-         passwordError: "",
+         passwordError: false,
          hasEmailError: false,
       };
    }
@@ -19,10 +21,9 @@ export default class SignUpCard extends React.Component {
       });
    }
 
-   validateAndCreateUser() {
-      const emailInput = document.getElementById("email-input").value;
-      //eslint-disable-next-line
+   async setEmailState(emailInput) {
       const loweredCasedEmailInput = emailInput.toLowerCase();
+      //eslint-disable-next-line
       const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
       if (emailInput === "")
@@ -40,6 +41,69 @@ export default class SignUpCard extends React.Component {
          this.setState({ emailError: "", hasEmailError: false });
       }
    }
+
+   checkHasLocalPart(passwordInput, emailInput) {
+      const localPart = emailInput.split("@")[0];
+      console.log(localPart);
+      if (localPart === "") return false;
+      else if (localPart.length < 4) return false;
+      else return passwordInput.includes(localPart);
+      //.includes will return a true or false
+   }
+
+   async setPasswordState(passwordInput, emailInput) {
+      console.log(passwordInput);
+
+      const uniqChars = [...new Set(passwordInput)];
+
+      if (passwordInput === "") {
+         this.setState({
+            passwordError: "Please create a password.",
+            hasPasswordError: true,
+         });
+      } else if (passwordInput.length < 9) {
+         this.setState({
+            passwordError: "Your password must be at least 9 characters.",
+            hasPasswordError: true,
+         });
+      } else if (this.checkHasLocalPart(passwordInput, emailInput)) {
+         this.setState({
+            passwordError:
+               "FOR YOUR SAFETY, Your password cannot contain your email address.",
+            hasPasswordError: true,
+         });
+      } else if (uniqChars.length < 3) {
+         this.setState({
+            passwordError:
+               "password must containe at least 3 unique characters",
+            hasPasswordError: true,
+         });
+      } else {
+         this.setState({ passwordError: "", hasPasswordError: false });
+      }
+   }
+
+   async validateAndCreateUser() {
+      const emailInput = document.getElementById("email-input").value;
+      const passwordInput = document.getElementById("password-input").value;
+      await this.setEmailState(emailInput);
+      await this.setPasswordState(passwordInput, emailInput);
+      if (
+         this.state.hasEmailError === false &&
+         this.state.hasPasswordError === false
+      ) {
+         const user = {
+            id: getUuid(),
+            email: emailInput,
+            password: hash(passwordInput),
+            createdAt: Date.now(),
+         };
+         console.log(user);
+      }
+   }
+   //click "lets go" going to run the function validateAndCreateUser, validateAndCreateUser
+   //is going to take in an email input, pass it through validateEmail (change the state)
+   //validateAndCreateUser breaks it down
 
    render() {
       return (
@@ -77,6 +141,7 @@ export default class SignUpCard extends React.Component {
                               <input
                                  type="email"
                                  className={classnames({
+                                    "mb-2": true,
                                     "form-control": true,
                                     "is-invalid": this.state.hasEmailError,
                                  })}
@@ -84,7 +149,7 @@ export default class SignUpCard extends React.Component {
                               />
                               {this.state.hasEmailError !== "" && (
                                  <p className="text-danger">
-                                    {this.state.emailError}
+                                    {this.state.passwordError}
                                  </p>
                               )}
                            </div>
@@ -97,9 +162,19 @@ export default class SignUpCard extends React.Component {
                               </label>
                               <input
                                  type="password"
-                                 className="form-control"
+                                 className={classnames({
+                                    "form-control": true,
+                                    "mb-2": true,
+                                    "is-invalid": this.state.hasPasswordError,
+                                 })}
                                  id="password-input"
                               />
+
+                              {this.state.hasPasswordError && (
+                                 <p className="text-danger">
+                                    {this.state.passwordError}
+                                 </p>
+                              )}
                            </div>
                            <button
                               className="btn btn-success btn-landing btn-block mt-5"
